494e5656e80d2fe4b255457e6055a408875ae9b0
Md Shariful Islam <devshariful99@gmail.com>
2026-02-16 10:41:39 +0600

    Update app.blade.php

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -38,18 +38,6 @@
     </script>
     @stack('styles')
     @stack('head_scripts')
-
-    {{-- Google tag (gtag.js) --}}
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-F0VX14ZLZ9"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-        gtag('config', 'G-F0VX14ZLZ9');
-    </script>
 </head>
 
 <body class="min-h-screen scroll-smooth flex flex-col bg-white">
